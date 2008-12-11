2974508aaff7ad19d2f7fa5c745a88b8eaa59018
Filodej <philodej@gmail.com>
2008-12-11 06:55:44 -0500

    Optimized the factories and extended the set of benchmarks.

--- bin/test.py
+++ bin/test.py
@@ -11,43 +11,34 @@ def deref( ptr ):
     except:
 	return ptr
 
-class adapter_factory:
+def _make_echo_adapter( base ):
+    class _adapter( base ):
+        def __init__( self, next ):
+            base.__init__( self )
+	    self._next = next
+	def call( self, val ):
+	    return self._next.call( val )
+    return _adapter
+
+class adapter_factory( object ):
+    echo_int = _make_echo_adapter( pyecho.echo_int )
+    echo_str = _make_echo_adapter( pyecho.echo_str )
+    
     def __init__( self, factory = pyecho ):
 	self.factory = factory
 	
     def create_echo_int( self ):
-	class adapter( pyecho.echo_int ):
-	    def __init__( self, next ):
-		pyecho.echo_int.__init__( self )
-		self._next = next
-	    def __getattribute__( self, name ):
-		if name[0] == '_':
-		    return pyecho.echo_int.__getattribute__(self, name) 
-		return getattr( self._next, name )		
-	return adapter( self.factory.create_echo_int() )
+	return adapter_factory.echo_int( self.factory.create_echo_int() )
 	
     def create_echo_str( self ):
-	class adapter( pyecho.echo_str ):
-	    def __init__( self, next ):
-		pyecho.echo_str.__init__( self )
-		self._next = next
-	    def __getattribute__( self, name ):
-		if name[0] == '_':
-		    return pyecho.echo_str.__getattribute__(self, name) 
-		return getattr( self._next, name )		
-	return adapter( self.factory.create_echo_str() )
+	return adapter_factory.echo_str( self.factory.create_echo_str() )
 
-class remote_factory:
+class remote_factory( object ):
     def __init__( self, host, port ):
 	self.conn = rpyc.classic.connect( host, port )
-	#self.create_echo_int = new.instancemethod( self.conn.modules["pyecho"].create_echo_int, self, self.__class__)
-	#self.create_echo_str = new.instancemethod( self.conn.modules["pyecho"].create_echo_str, self, self.__class__)
-
-    def create_echo_int( self ): 
-	return self.conn.modules["pyecho"].create_echo_int()
-	
-    def create_echo_str( self ): 
-	return self.conn.modules["pyecho"].create_echo_str()
+	mod = self.conn.modules["pyecho"]
+	self.create_echo_int = mod.create_echo_int
+	self.create_echo_str = mod.create_echo_str
 	
 def simple_test( factory = pyecho ):
     ei = factory.create_echo_int()
@@ -56,7 +47,7 @@ def simple_test( factory = pyecho ):
     es = factory.create_echo_str()
     assert '42' == es.call( '42' )
 
-def advanced_test( factory = pyecho, treshold = 5.0 ):
+def advanced_test( factory = pyecho, treshold = 5.0, verbose = False ):
     def benchmark( echo, fn, val ):
 	i = 10
 	while True:
@@ -64,12 +55,15 @@ def advanced_test( factory = pyecho, treshold = 5.0 ):
 	    t = fn( deref(echo), val, 2**i )
 	    if t > treshold:
 		break
-	print '\t\t%f calls/sec.\n\t\t\t(%d calls in %f seconds)' % ( float(2**i)/t, 2**i, t )
+	print '%f calls/sec.' % ( float(2**i)/t )
+	if verbose:
+    	    print '\t\t(%d calls in %f seconds)' % ( 2**i, t )
 
-    print '\tcall(int)'
+    print '\tcall(int)\t\t',
     benchmark( factory.create_echo_int(), pyecho.benchmark_int, 42 )
-    print '\tcall(str[len=42])'
-    benchmark( factory.create_echo_str(), pyecho.benchmark_str, 42 * 'X' )
+    for i in range( 3, 8 ):
+        print '\tcall(str[len=%d])\t' % ( 2**i ),
+	benchmark( factory.create_echo_str(), pyecho.benchmark_str, 2**i * 'X' )
     
 g_usage = """\
 usage:
@@ -79,11 +73,13 @@ options:
     -m, --mode     ... mode (one of 'local', 'client', server)
     -H, --hostname ... rpyc server hostname (default 'localhost')
     -p, --port     ... rpyc port (default 18861)
+    -t, --treshold ... benchmark treshold (default 5.0 sec.)
+    -v, --verbose  ... detailed test results printed (disabled by default)
 """
 
 def parse_options():
     try:
-	opts, args = getopt.getopt(sys.argv[1:], "hH:m:p:", ["help", "hostname=", "mode=", "port="])
+	opts, args = getopt.getopt(sys.argv[1:], "hH:m:p:t:v", ["help", "hostname=", "mode=", "port=", "treshold=", "verbose"])
     except getopt.GetoptError, err:
 	print str(err) # will print something like "option -a not recognized"
 	print g_usage
@@ -91,12 +87,18 @@ def parse_options():
     hostname = "localhost"
     port = 18861
     mode = 'local'
+    treshold = 5.0
+    verbose = False
     for o, a in opts:
         if o in ("-h", "--help"):
 	    print g_usage
 	    sys.exit()
 	elif o in ("-p", "--port"):
 	    port = a
+	elif o in ("-t", "--treshold"):
+	    treshold = float(a)
+	elif o in ("-v", "--verbose"):
+	    verbose = True
 	elif o in ("-h", "--hostname"):
 	    hostname = a
 	elif o in ("-m", "--mode"):
@@ -107,11 +109,11 @@ def parse_options():
 	    mode = a
 	else:
 	    assert False, "unhandled option"
-    return mode, hostname, port
+    return mode, hostname, port, treshold, verbose
     
     
 if __name__ == '__main__':
-    mode, hostname, port = parse_options()
+    mode, hostname, port, treshold, verbose = parse_options()
     if mode == 'server':
         ts = rpyc.utils.server.ThreadedServer( rpyc.SlaveService, port = port )
 	ts.start()
@@ -124,4 +126,4 @@ if __name__ == '__main__':
 	for desc, f in factories:
 	    print desc
 	    simple_test( f )
-	    advanced_test( f )
+	    advanced_test( f, treshold, verbose )
